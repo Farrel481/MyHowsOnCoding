c385a8d916e3afb5e96c0e1acb57be6291571fd6
Farrel481 <Farrelsatriano2@gmail.com>
2026-04-21 17:36:07 +0700

    Update Notes

---
+++ How-to-Python/BaseSyntax.py
@@ -0,0 +1,21 @@
+#Printing
+print("Hello World")
+print('Hello \nEveryone!')
+print('Hello' + 'Farrel')
+print("Kota", 'Bandung', "Barat")
+print("Kota", 'Jakarta', "Selatan", sep="-")
+
+print("Satu"
+      "Dua"
+      "Tiga")
+
+#Variable
+name = "Farrel"
+age = 17
+
+print("Hello ", name, ", Kamu apakabar?")
+print("Hello " + name + ", Kamu apakabar?")
+print(f"Hello {name}, Kamu apakabar?")
+print("Hello {}, Kamu apakabar?".format(name))
+#Bisa juga kaya bahasa C/C++ nih
+print('Hello %s, Umur kamu %d tahun ya?' % (name, age))
