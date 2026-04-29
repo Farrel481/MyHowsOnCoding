e2a81fdd33893fd1e2b9206f9c914776163d3e7b
Farrel481 <Farrelsatriano2@gmail.com>
2026-04-29 23:54:47 +0700

    OOP start

--- How-to-Python/OOP.py
+++ How-to-Python/OOP.py
@@ -1,3 +1,40 @@
 #OOP is simply Object Oriented Programming.
 #Artinya OOP itu programming yang terfokus pada sebuah object atau tujuan tertentu.
 
+#Today Commit at 28/04/2026 we will learn about class
+
+#Class Static
+class Binatang:
+    jumlahKaki = 4          #properti
+    habitat = 'Darat'       #properti  
+    def suara(self):        #method dan self ini a must untuk first parameter
+        return 'Mengaum'
+
+anjing = Binatang()
+print(anjing.jumlahKaki)
+print(anjing.habitat)
+print(anjing.suara())
+
+kucing = Binatang()
+print(kucing.jumlahKaki)
+print(kucing.habitat)
+print(kucing.suara())
+
+#Contoh penggunaan yang salah
+elang = Binatang()
+print(elang.jumlahKaki) 
+print(elang.habitat)
+print(elang.suara()) 
+
+
+class kelasKosong:
+    pass
+
+o = kelasKosong()
+print(o)
+
+class Player:
+    name = 'Andi'
+    def run(self):
+        return f"run {self.name}"
+    
