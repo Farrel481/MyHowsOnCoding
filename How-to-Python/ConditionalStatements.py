e91bc0c466b33043acbcaecb3475c737b830ce5f
Farrel481 <Farrelsatriano2@gmail.com>
2026-07-18 05:24:13 +0700

    Statements

---
+++ How-to-Python/ConditionalStatements.py
@@ -0,0 +1,24 @@
+#IF ELSE STATEMENTS
+
+#if {conditions}:
+#    {code}
+
+nilai = 6
+#student lulus jika nilai >= 7
+
+if nilai >= 7:
+    print("Anda lulus!")
+    print("Nilai Anda", nilai)
+else:
+    print("Anda TIDAK LULUS.")
+    print("Nilai Anda", nilai)
+
+x = 10
+y = 1
+if x is y:
+    print("x sama dengan y ")
+else:
+    print("x tidak sama dengan y")
+
+nama = "Farrel"
+print(f"Hello nama saya {nama}!")
