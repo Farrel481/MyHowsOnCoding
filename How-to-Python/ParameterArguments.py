ed863f1269019ecb4f778a84fad3fe9d15f822e5
Farrel481 <Farrelsatriano2@gmail.com>
2026-07-18 15:30:41 +0700

    ParaArgs

---
+++ How-to-Python/ParameterArguments.py
@@ -0,0 +1,10 @@
+#f(x) = 3x + 2
+#From this data wecan say x as Parameter and 3x + 2 as Argument
+
+#Parameter is something defined in functions
+#Argument is a value that is sent to the parameter when specified func is called
+
+nama = input("Nama mu siapa?")
+def func(nama):
+    print(f"Hello {nama}!")
+func(nama)
