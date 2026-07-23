b052bfefd4de9ebcb320f9d43237fd2f8939e667
Farrel481 <Farrelsatriano2@gmail.com>
2026-07-23 19:20:27 +0700

    new

--- How-to-Python/ParameterArguments.py
+++ How-to-Python/ParameterArguments.py
@@ -4,7 +4,10 @@
 #Parameter is something defined in functions
 #Argument is a value that is sent to the parameter when specified func is called
 
-nama = input("Nama mu siapa?")
+nama = input("Nama mu siapa? ")
 def func(nama):
     print(f"Hello {nama}!")
+#func() TypeError: func() missing 1 required positional argument: 'nama'
 func(nama)
+
+#POSITIONAL ARGUMENTS
