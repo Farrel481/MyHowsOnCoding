cb783e88736f55996f95b3d12dc8729b9cedf4b9
Farrel481 <Farrelsatriano2@gmail.com>
2026-07-25 11:03:17 +0700

    Json Strings

---
+++ How-to-Python/file_handle/7_strjson.py
@@ -0,0 +1,31 @@
+import json
+
+#JSON data format filled with str
+data = '{"name": "Farrel", "city": "Unknown"}' #most will miss the " ' ".
+print(data)
+print(type(data))
+
+dataJson = json.loads(data) #str json as an object equals py dict
+print(dataJson)
+print(type(dataJson))
+
+data2 = '[12, 24, 48, 96, 192]' #str json as an array equals py list
+data2Json = json.loads(data2)
+print(data2Json)
+print(type(data2Json))
+
+data3 = '{"count": [1, 2, 3]}'
+data3Json = json.loads(data3)
+print(data3Json)
+print(type(data3Json))
+
+data4 = '[1, 2, {"Me": "Myself"}]'
+data4Json = json.loads(data4)
+print(data4Json)
+print(type(data4Json))
+
+#Invalid Json variable
+#data5 =  '{"name": "Agus", "age": 21, "0": true, "x": null,}'
+# data5Json = json.loads(data5)
+# print(data5Json)
+# print(type(data5Json))
