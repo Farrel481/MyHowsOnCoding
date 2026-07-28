999b83630e41a12ae3d19b84b1d11a58533275cf
Farrel481 <Farrelsatriano2@gmail.com>
2026-07-28 20:46:07 +0700

    Update PostgreSQL progress 2026-07-28 20:46

--- Data-Workloads/1_Database/Belajar PostgreSQL/queries/1_ddl&dml.sql
+++ Data-Workloads/1_Database/Belajar PostgreSQL/queries/1_ddl&dml.sql
@@ -15,4 +15,25 @@ ALTER TABLE users ADD COLUMN isMarried BOOL DEFAULT FALSE;
 INSERT INTO users (nama, usia, kota) VALUES ('Gilang', 18, 'Jakarta');
 INSERT INTO users (nama, usia, kota) VALUES ('Budi', 28, 'Purwakarta') RETURNING
 
+-- DML (Data Manipulation Language)
+-- With select
+SELECT * from users;
+
+SELECT nama, usia from users;
+
+SELECT * FROM users WHERE kota = 'Jakoarta';
+
+SELECT * FROM users WHERE usia < 28;
+SELECT * FROM users WHERE usia > 28 AND kota = 'Jakarta';
+SELECT * FROM users WHERE usia > 28 OR kota = 'Jakarta';
+
+SELECT * FROM users LIMIT 3;
+SELECT * FROM users LIMIT 3 OFFSET;
+
+SELECT * FROM users ORDER BY id;
+SELECT * FROM users ORDER BY id ASC;
+SELECT * FROM users ORDER BY id DESC;
+
+
+
 
